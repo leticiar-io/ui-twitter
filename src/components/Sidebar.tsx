0be1832460130bd82ca5a2c7d943321000aa609f
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-03-05 11:44:33 -0300

    separating components

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,61 @@
+import twitterLogo from "../assets/logo.svg";
+import {
+  House,
+  Hash,
+  Bell,
+  Envelope,
+  BookmarkSimple,
+  FileText,
+  User,
+  DotsThreeCircle,
+  Sparkle,
+} from "phosphor-react";
+
+import './Sidebar.css';
+
+export function Sidebar() {
+  return (
+    <aside className="sidebar">
+      <img className="logo" src={twitterLogo} alt="Logo" />
+
+      <nav className="main-navigation">
+        <a className="active" href="">
+          <House weight="fill" />
+          Home
+        </a>
+        <a href="">
+          <Hash />
+          Explore
+        </a>
+        <a href="">
+          <Bell />
+          Notifications
+        </a>
+        <a href="">
+          <Envelope />
+          Messages
+        </a>
+        <a href="">
+          <BookmarkSimple />
+          Bookmarks
+        </a>
+        <a href="">
+          <FileText />
+          Lists
+        </a>
+        <a href="">
+          <User />
+          Profile
+        </a>
+        <a href="">
+          <DotsThreeCircle />
+          More
+        </a>
+      </nav>
+
+      <button className="new-tweet" type="button">
+        Tweet
+      </button>
+    </aside>
+  );
+}
