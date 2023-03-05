8e329993459a84bae4af845416fd86d06c2f342b
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-03-05 11:02:27 -0300

    creating the sidebar

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,67 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 import "./global.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import twitterLogo from "./assets/logo.svg";
+import {
+  House,
+  Hash,
+  Bell,
+  Envelope,
+  BookmarkSimple,
+  FileText,
+  User,
+  DotsThreeCircle,
+} from "phosphor-react";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <div className="layout">
-      <aside className="sidebar"></aside>
+      <aside className="sidebar">
+        <img className="logo" src={twitterLogo} alt="Logo" />
 
-      <div className="content">
-        
-      </div>
+        <nav className="main-navigation">
+          <a className="active" href="">
+            <House weight="fill" />
+            Home
+          </a>
+          <a href="">
+            <Hash />
+            Explore
+          </a>
+          <a href="">
+            <Bell />
+            Notifications
+          </a>
+          <a href="">
+            <Envelope />
+            Messages
+          </a>
+          <a href="">
+            <BookmarkSimple />
+            Bookmarks
+          </a>
+          <a href="">
+            <FileText />
+            Lists
+          </a>
+          <a href="">
+            <User />
+            Profile
+          </a>
+          <a href="">
+            <DotsThreeCircle />
+            More
+          </a>
+        </nav>
+
+        <button className="new-tweet" type="button">
+          Tweet
+        </button>
+      </aside>
+
+      <div className="content">content</div>
     </div>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
